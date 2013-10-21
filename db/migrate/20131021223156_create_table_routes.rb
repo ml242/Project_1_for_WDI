c6c2a6497e58bb58a843b919600fc6532b0c4b24
Matt Lucas <mattlucas@gmail.com>
2013-10-21 19:01:40 -0400

    tabling

--- db/migrate/20131021223156_create_table_routes.rb
+++ db/migrate/20131021223156_create_table_routes.rb
@@ -1,5 +1,13 @@
 class CreateTableRoutes < ActiveRecord::Migration
   def change
-
+    create_table :routes do |t|
+      t.string :name
+      t.integer :route_id
+      t.string :face_location
+      t.text :description
+      t.integer :danger
+      t.string :grade
+      t.text :comments
+    end
   end
 end
