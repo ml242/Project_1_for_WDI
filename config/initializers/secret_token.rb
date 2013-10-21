d2d3666f01d2d0a23518c77a9e88376eb54d4a44
Matt Lucas <mattlucas@gmail.com>
2013-10-21 18:25:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrandSendStation::Application.config.secret_token = '299554f57e8dd0ca80ec39f6ced16c4fe4935b14aa2f293f41bc6e9ba34892505f8700bebc9d817a406a8d8cfa8e5d44b28d89c84ca6f62bca3b848a95cd51f4'
