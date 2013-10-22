19811bddb2150d85d1edf56b5f7a6b91e054f50f
Matt Lucas <mattlucas@gmail.com>
2013-10-22 19:24:33 -0400

    yea yeagit add .git add .git add .

--- app/models/ascend.rb
+++ app/models/ascend.rb
@@ -2,4 +2,5 @@ class Ascend < ActiveRecord::Base
   has_many :climbs
   has_many :users, :through => :climbs
   belongs_to :crag
+  attr_accessible :name, :grade, :crag_id
 end
--- app/models/crag.rb
+++ app/models/crag.rb
@@ -1,3 +1,4 @@
 class Crag < ActiveRecord::Base
   has_many :ascends
+  attr_accessible :name
 end
