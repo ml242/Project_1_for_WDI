4897dac9d7dadeee21960fd6d9476d3135bfba37
Matt Lucas <mattlucas@gmail.com>
2014-03-01 21:49:58 -0500

    working on odd problem with user_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
+
   before_filter :authenticate
+
   def index
     @users = User.all
     @ascends = Ascend.all
   end
+
   def new
     # render users/new signup form
     @user = User.new
   end
+
   # POST /users
   def create
     @user = User.new(params[:user])
@@ -20,16 +24,19 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def edit
     # @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
-    redirect_to(@user)
+    user = User.find(params[:id])
+    user.update_attributes(params[:user])
+    redirect_to user_path
   end
 
 
