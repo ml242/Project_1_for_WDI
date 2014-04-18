ecc07402547bef16533b23d47ce55438d888c555
ml242 <mattlucas@gmail.com>
2014-04-18 09:37:59 -0400

    canvas is not playing nicely with paperscript

--- app/assets/javascripts/myPaperRoutes.js
+++ app/assets/javascripts/myPaperRoutes.js
@@ -6,23 +6,24 @@ window.onload = function() {
 		paper.setup(canvas);
 		// Create a Paper.js Path to draw a line into it:
 		var path = new paper.Path();
-		// // Give the stroke a color
+		// Give the stroke a color
 		path.strokeColor = 'black';
+		
 		var start = new paper.Point(100, 100);
 		// Move to start and draw a line from there
 		path.moveTo(start);
 		// Note that the plus operator on Point objects does not work
 		// in JavaScript. Instead, we need to call the add() function:
-		// path.lineTo(start.add([ 200, -50 ]));
-		// Draw the view now:
-		paper.view.draw();
+		// path.lineTo(start.add([ 200, -500 ]));
 
 
 		// var textItem = new PointText({
 		// 	content: 'Click and drag to draw a line.',
-		// 	point: new Point(20, 30),
+		// 	point: new Point(200, 300),
 		// 	fillColor: 'black',
 		// });
+
+		// var path;
 
 		function onMouseDown(event) {
 	// If we produced a path before, deselect it:
@@ -42,26 +43,28 @@ window.onload = function() {
 // While the user drags the mouse, points are added to the path
 // at the position of the mouse:
 function onMouseDrag(event) {
-	path.add(event.point);
+		path.add(event.point);
 
-	// Update the content of the text item to show how many
-	// segments it has:
-	textItem.content = 'Segment count: ' + path.segments.length;
-}
-
-// When the mouse is released, we simplify the path:
-function onMouseUp(event) {
-	var segmentCount = path.segments.length;
-
-	// When the mouse is released, simplify it:
-	path.simplify(10);
-
-	// Select the path, so we can see its segments:
-	path.fullySelected = true;
-
-	var newSegmentCount = path.segments.length;
-	var difference = segmentCount - newSegmentCount;
-	var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-	textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+		// Update the content of the text item to show how many
+		// segments it has:
+		textItem.content = 'Segment count: ' + path.segments.length;
 	}
+
+	// When the mouse is released, we simplify the path:
+	function onMouseUp(event) {
+		var segmentCount = path.segments.length;
+
+		// When the mouse is released, simplify it:
+		path.simplify(10);
+
+		// Select the path, so we can see its segments:
+		path.fullySelected = true;
+
+		var newSegmentCount = path.segments.length;
+		var difference = segmentCount - newSegmentCount;
+		var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
+		textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+	}
+
+
 };
