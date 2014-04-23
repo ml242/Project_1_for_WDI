001001b7c8a5d49cddea7f96d190054973152972
ml242 <mattlucas@gmail.com>
2014-04-23 09:10:18 -0400

    adjusted the seeds for moar

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,30 @@
 # FIXME remove hard coded ids and associate using activerecord relationships. c1.ascends << a1
 
 
-
+#south park
 crag0 = Crag.create(latitude: 40.769333, longitude: -73.977639, name: 'Rat Rock', description: 'Rat Rock is the premiere location to boulder in Central Park. On a given day you can find local residents who have been at the crag for decades as well as tourists taking a break from New York\'s other offerings to enjoy the park. The scene here is as friendly as it is storied. Below are most of the featured routes as well as where they reside on the rock.')
-crag1 = Crag.create(latitude:  40.768288, longitude: -73.973694, name: 'Cat Rock', description: "Cat Rock is a sunny crag with a variety of routes. Some can be a little sketchy, bring a partner and crash pad!")
-crag2 = Crag.create(latitude:  40.767532, longitude: -73.973648, name: 'Tooth Rock', description: "Tooth Rock a small molar shaped boulder with some tricky routes but right on a major pathway.")
-crag3 = Crag.create(latitude:  40.768997, longitude: -73.974754, name: 'Chess Rock', description: "Chess Rock is a small assortment of easier friction climbs await you here.")
+crag1 = Crag.create(latitude: 40.768288, longitude: -73.973694, name: 'Cat Rock', description: "Cat Rock is a sunny crag with a variety of routes. Some can be a little sketchy, bring a partner and crash pad!")
+crag2 = Crag.create(latitude: 40.767532, longitude: -73.973648, name: 'Tooth Rock', description: "Tooth Rock a small molar shaped boulder with some tricky routes but right on a major pathway.")
+crag3 = Crag.create(latitude: 40.768997, longitude: -73.974754, name: 'Chess Rock', description: "Chess Rock is a small assortment of easier friction climbs await you here.")
+#central park
+crag4 = Crag.create(latitude: 40.777927, longitude: -73.971115, name: 'Hepatitis Boulder', description: 'Swampy and challenging climbs off the beaten path.')
+crag5 = Crag.create(latitude: 40.778259, longitude: -73.971352, name: 'Arch Rock', description: 'There are some nice climbs at this rock, bring a buddy and a pad to spot!')
+crag6 = Crag.create(latitude: 40.775963, longitude: -73.968895, name: 'Freestanding Rock', description: 'A small boulder with a few warmups in a peaceful setting.')
+crag7 = Crag.create(latitude: 40.776009, longitude: -73.968895, name: 'Split Rock', description: 'One massive sloping boulder cleaved in half leaves a lot of options for new routes and traverses.')
+#northside
+crag7 = Crag.create(latitude:  40.795715, longitude: -73.954231, name: 'Laskar Rock', description: '')
+crag7 = Crag.create(latitude:  40.796268, longitude: -73.960930, name: 'Westside Outcrops', description: '')
+crag7 = Crag.create(latitude:   40.798836, longitude: -73.954727, name: 'Worthless Boulder', description: '')
+crag7 = Crag.create(latitude: 40.776009, longitude: -73.955688, name: 'Shit Rock', description: '')
+crag7 = Crag.create(latitude:  40.799370, longitude: -73.957405, name: 'Roadside Boulder', description: '')
 
+
+#cat
 a1 = Ascend.create(name: 'Hissy Fit', grade: 'V1', crag_id: crag1.id)
 a1 = Ascend.create(name: 'The Prophet', grade: 'V10', crag_id: crag1.id)
 a1 = Ascend.create(name: 'Hissy Fit', grade: 'V1', crag_id: crag1.id)
 a1 = Ascend.create(name: 'The Dawg', grade: 'V5', crag_id: crag1.id)
-
-a2 = Ascend.create(name: 'Top Seam', grade: 'V0-', crag_id: crag2.id)
-a2 = Ascend.create(name: 'Watch Your Back', grade: 'V3', crag_id: crag2.id)
-a2 = Ascend.create(name: 'Cavity Crack', grade: 'V0', crag_id: crag2.id)
-
-a3 = Ascend.create(name: 'Check Mate', grade: 'V0-', crag_id: crag3.id)
-a3 = Ascend.create(name: 'Chess Rock Traverse', grade: 'V0-', crag_id: crag3.id)
-a6 = Ascend.create(name: 'Knight\'s Move', grade: 'V2', face_location: 'West', crag_id: crag3.id)
-
+#rat
 a4 = Ascend.create(name: 'Rat Rock Traverse', grade: 'VO', face_location: 'North', crag_id: crag0.id)
 a4 = Ascend.create(name: 'High Traverse', grade: 'V1', face_location: 'North', crag_id: crag0.id)
 a4 = Ascend.create(name: 'Low Traverse', grade: 'V2', face_location: 'North', crag_id: crag0.id)
@@ -63,7 +68,14 @@ a4 = Ascend.create(name: 'Master Ling', grade: 'V7', face_location: 'East', crag
 a4 = Ascend.create(name: 'Death by Dizzy', grade: 'V2', face_location: 'East', crag_id: crag0.id)
 a4 = Ascend.create(name: 'Bottom Line', grade: 'V9', face_location: 'East', crag_id: crag0.id)
 a4 = Ascend.create(name: 'Butter', grade: 'V2', face_location: 'East', crag_id: crag0.id)
+#tooth
+a2 = Ascend.create(name: 'Top Seam', grade: 'V0-', crag_id: crag2.id)
+a2 = Ascend.create(name: 'Watch Your Back', grade: 'V3', crag_id: crag2.id)
+a2 = Ascend.create(name: 'Cavity Crack', grade: 'V0', crag_id: crag2.id)
+#chess
+a3 = Ascend.create(name: 'Check Mate', grade: 'V0-', crag_id: crag3.id)
+a3 = Ascend.create(name: 'Chess Rock Traverse', grade: 'V0-', crag_id: crag3.id)
+a6 = Ascend.create(name: 'Knight\'s Move', grade: 'V2', face_location: 'West', crag_id: crag3.id)
 
-# a5 = Ascend.create(name: 'Top Seam', grade: 'V0-', face_location: 'West', crag_id: crag2.id)
 
 a11 = Weather.create(current_temp: 99, zip: '11101')
