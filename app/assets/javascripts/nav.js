b3fbad1672ac056502efab63dc9ef6e61d7f26a4
ml242 <mattlucas@gmail.com>
2014-04-24 00:42:17 -0400

    erroring on validating the user in ActiveRecord but so close

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,3 +1,69 @@
+createUser = function(e) {
+  e.preventDefault();
+
+  // this is the params that will go into the ajax
+  var userParams = {
+    user: {
+      latitude: userLat,
+      longitude: userLng
+  }
+};
+
+  // ajax request to add data userParams into the data
+  $.ajax({
+    type: "POST",
+    url: "users.json",
+    data: userParams
+  }).done(function(user) {
+    // do something with the response
+    user = user;
+    console.log('response:');
+    console.log(user);
+    app.updateLocation();
+  });
+};
+
+
+updateLocation = function() {
+
+  // continuously checks your position and calls the function
+  // watchPosition is an evenlistener on geoloc so whenever user moves, it triggers and calls success function
+  navigator.geolocation.watchPosition(function(position) {
+    console.log(position);
+    userLat = position.coords.latitude;
+    userLng = position.coords.longitude;
+
+  // this is the params that will go into the ajax
+  var a = $('#user-id').text();
+	var user_id = parseInt(a);
+  
+  var userParams = {
+    user: {
+      id: user_id,
+      latitude: userLat,
+      longitude: userLng
+    }
+  };
+
+
+  var updateUserUrl = "users/" + user_id;
+
+// updates users lat long through ajax request
+    $.ajax({
+      dataType: "json",
+      type: "PUT",
+      url: updateUserUrl,
+      data: userParams
+    }).done(function(user){
+      user = user;
+      console.log(user);
+    });
+  });
+
+};
+
+
+
 // document ready
 $(document).ready(function() {
 
@@ -19,5 +85,6 @@ $(document).ready(function() {
 // $(".submit").on("click", app.createUser);
 
 	setInterval(1000);
+	updateLocation();
 
 });
