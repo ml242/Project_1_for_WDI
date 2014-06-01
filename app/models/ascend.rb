4b226ebfdcf0994dc1880287f3641305c14ed980
ml242 <mattlucas@gmail.com>
2014-06-01 15:13:07 -0400

    rolling back the migration for addAttachment

--- app/models/ascend.rb
+++ app/models/ascend.rb
@@ -1,9 +1,10 @@
 
 
 class Ascend < ActiveRecord::Base
-  attr_accessible :name, :face_location, :description, :is_dangerous, :grade, :comments, :crag_id, :crag_base_image_file_name, :crag_edited_image_file_name
-  has_attached_file :ascend_base_image_file_name, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  has_attached_file :ascend_edited_image_file_name, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  attr_accessible :name, :face_location, :description, :is_dangerous, :grade, :comments, :crag_id, 
+  # :crag_base_image_file_name, :crag_edited_image_file_name
+  # has_attached_file :ascend_base_image_file_name, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # has_attached_file :ascend_edited_image_file_name, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   # validates_attachment_content_type :ascend_base_image_file_name, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   # validates_attachment_content_type :ascend_edited_image_file_name, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   has_many :climbs
